bd1670d4bf4c1292266d688112eb93e5a77734a8
ThatNoam <noamk574@gmail.com>
2026-02-21 17:24:28 +0200

    final commit

--- app/src/main/java/com/example/e010/MainActivity.java
+++ app/src/main/java/com/example/e010/MainActivity.java
@@ -14,7 +14,7 @@ public class MainActivity extends AppCompatActivity {
 
 EditText display;
 String text;
-int num = 0;
+double num = 0.0;
 String lastop = "";
 
     @Override
@@ -28,20 +28,68 @@ String lastop = "";
     private void calculate() {
         text = display.getText().toString();
 
+        if (text.isEmpty())
+        {
+            return;
+        }
+
         if (lastop.equals("plus"))
         {
-            num = num + Integer.parseInt(text);
+            num = num + Double.parseDouble(text);
         }
+        else if (lastop.equals("minus"))
+        {
+            num = num - Double.parseDouble(text);
+        }
+        else if (lastop.equals("mult"))
+        {
+            num = num * Double.parseDouble(text);
+        }
+        else if (lastop.equals("div"))
+        {
+            if (Double.parseDouble(text) != 0.0)
+            {
+                num = num / Double.parseDouble(text);
+            }
+        }
+
         else
         {
-            num = Integer.parseInt(text);
+            num = Double.parseDouble(text);
         }
+    }
+
+    public void clear(View view) {
+        num = 0.0;
+        lastop = "";
+        display.setText("");
     }
 
     public void plusnum(View view)
     {
         calculate();
         lastop = "plus";
+        display.setText("");
+    }
+
+    public void minusnum(View view)
+    {
+        calculate();
+        lastop = "minus";
+        display.setText("");
+    }
+
+    public void multnum(View view)
+    {
+        calculate();
+        lastop = "mult";
+        display.setText("");
+    }
+
+    public void divnum(View view)
+    {
+        calculate();
+        lastop = "div";
         display.setText("");
     }
 
