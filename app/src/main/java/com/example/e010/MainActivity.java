e80aa30ff269cf9c42a7f0c1e043f8ea45604acc
ThatNoam <noamk574@gmail.com>
2026-02-21 17:14:56 +0200

    3rd commit

--- app/src/main/java/com/example/e010/MainActivity.java
+++ app/src/main/java/com/example/e010/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.e010;
 
 import android.os.Bundle;
-
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
@@ -10,9 +12,44 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+EditText display;
+String text;
+int num = 0;
+String lastop = "";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        display = findViewById(R.id.editTextNumber);
     }
+
+    private void calculate() {
+        text = display.getText().toString();
+
+        if (lastop.equals("plus"))
+        {
+            num = num + Integer.parseInt(text);
+        }
+        else
+        {
+            num = Integer.parseInt(text);
+        }
+    }
+
+    public void plusnum(View view)
+    {
+        calculate();
+        lastop = "plus";
+        display.setText("");
+    }
+
+    public void equals(View view)
+    {
+        calculate();
+        display.setText(String.valueOf(num));
+        lastop = "";
+    }
+
 }
